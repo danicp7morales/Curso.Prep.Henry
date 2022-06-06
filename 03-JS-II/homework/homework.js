e7438441ae9bd274eb4ce16d5a86adae5209557d
Dany <danielMoralescp7>
2022-06-06 20:43:42 -0300

    commit 04 JS III

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -165,7 +165,7 @@ function esPrimo(numero) {
   // Nota: Los números 0 y 1 NO son considerados números primos
   if(numero < 2) return false;
   if(numero === 2) return true;
-  for(let  i = 2; i < numero; i++){
+  for(let i = 2; i < numero; i++){
     if(numero % i === 0){
       return false;
     }
