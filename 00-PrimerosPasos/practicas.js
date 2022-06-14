7ca4d60596472b5b8bedf5a276753313aa2ff8d0
Dany <danielMoralescp7>
2022-06-14 18:51:23 -0300

    commit noche del 14/06/2022

--- 00-PrimerosPasos/practicas.js
+++ 00-PrimerosPasos/practicas.js
@@ -1,6 +1,36 @@
-const numeros = [45, 54, 73, 81, 7, 17, 99];
-let txt = "";
-for(let x in numeros){
-    txt += numeros[x];
-}
-return txt;
+const automoviles = [
+    { name:'Renault Megane', precio:16000 },
+    { name:'Toyota Corolla', precio:17000 },
+    { name:'Honda Civic', precio:18000 }
+];
+
+// ES5
+/*const precios = automoviles.map(function(automovil) {
+    return automoviles.precio;
+});
+
+return precios;*/
+
+const precios = automoviles.map(automovil => automovil.precio);
+return precios;
+
+
+
+
+
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+// ES5
+var divisibleEntreCuatroES5 = array.filter(function (n){
+    return n % 4 === 0;
+});
+
+// ES6
+const divisibleEntreCuatroES6 = array.filter(n => n % 4 === 0);
+
+console.log(divisibleEntreCuatroES6); // [4, 8, 12, 16, 20]
+
+
+
+;
